e21ed1840b2cb7032fb53ca173c9e3210bf66d13
Allexandere <39259989+Allexandere@users.noreply.github.com>
2021-03-10 01:24:54 +0300

    new solution

--- src/main/java/com/example/service/TranslateService.java
+++ src/main/java/com/example/service/TranslateService.java
@@ -20,6 +20,10 @@ public class TranslateService {
     }
 
     public Translation addTranslation(TranslationBody translationBody, String from, String to) {
+        /*
+        1)т.к. не знаю как реализовать поиск перевода, имея только одну запись в словаре нужного типа
+        2)т.к. не знаю как не добовлять новую запись, когда она уже присутствует
+         */
         Translation translation = new Translation(translationBody.getFrom_word(), translationBody.getTo_word(), from, to);
         Translation reversedTranslation = new Translation(translationBody.getTo_word(), translationBody.getFrom_word(), to, from);
         translateRepository.createTranslation(reversedTranslation);
